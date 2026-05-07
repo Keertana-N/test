601d4743383e99576bd57c052877f15e738d3b05
Keertana <nkeertana229@gmail.com>
2026-05-07 22:22:19 +0530

    added the newsapi integration

---
+++ app/routes/news.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+router = APIRouter()
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+
+@router.get("/financial-news")
+def get_financial_news():
+
+    url = (
+        f"https://newsapi.org/v2/top-headlines?"
+        f"category=business&language=en&apiKey={NEWS_API_KEY}"
+    )
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    articles = data.get("articles", [])
+
+    cleaned_articles = []
+
+    for article in articles[:5]:
+        cleaned_articles.append({
+            "title": article.get("title"),
+            "source": article.get("source", {}).get("name"),
+            "url": article.get("url")
+        })
+
+    return {
+        "status": "success",
+        "articles": cleaned_articles
+    }
