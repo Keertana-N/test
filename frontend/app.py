37633e7012b9b7077abcdad6239417cb18c907fe
Keertana <nkeertana229@gmail.com>
2026-05-13 21:28:03 +0530

    made a few changes to the ui

--- frontend/app.py
+++ frontend/app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 st.set_page_config(
     page_title="FinSight AI",
     layout="wide"
 )
 
+st.sidebar.title("FinSight AI")
+st.sidebar.write("Real-time financial sentiment tracker")
 st.title("📈 FinSight AI Dashboard")
 
 st.subheader("AI-Powered Financial News Sentiment Analysis")
@@ -16,21 +19,69 @@ response = requests.get(API_URL)
 
 data = response.json()
 
-st.write("### Sentiment Summary")
-st.json(data["sentiment_summary"])
+summary = data["sentiment_summary"]
 
-st.write("### Financial News Articles")
+articles = data["articles"]
 
-for article in data["articles"]:
+# ======================
+# METRICS
+# ======================
 
-    st.subheader(article["title"])
+col1, col2, col3 = st.columns(3)
 
-    st.write(f"Source: {article['source']}")
+col1.metric("🟢 Positive", summary["positive"])
 
-    st.write(f"Sentiment: {article['sentiment']}")
+col2.metric("🔴 Negative", summary["negative"])
 
-    st.write(f"Confidence Score: {article['confidence_score']}")
+col3.metric("⚪ Neutral", summary["neutral"])
 
-    st.markdown(f"[Read Article]({article['url']})")
+st.divider()
 
-    st.divider()
+# ======================
+# ARTICLES SECTION
+# ======================
+
+st.header("📰 Financial News")
+
+for article in articles:
+
+    sentiment = article["sentiment"]
+
+    if sentiment == "POSITIVE":
+        emoji = "🟢"
+
+    elif sentiment == "NEGATIVE":
+        emoji = "🔴"
+
+    else:
+        emoji = "⚪"
+
+    with st.container():
+
+        st.subheader(article["title"])
+
+        st.write(f"**Source:** {article['source']}")
+
+        st.write(
+            f"**Sentiment:** {emoji} {article['sentiment']}"
+        )
+
+        st.write(
+            f"**Confidence Score:** {article['confidence_score']}"
+        )
+
+        st.markdown(
+            f"[Read Full Article]({article['url']})"
+        )
+
+        st.divider()
+
+# ======================
+# DATAFRAME SECTION
+# ======================
+
+st.header("📊 Structured Data")
+
+df = pd.DataFrame(articles)
+
+st.dataframe(df)   
